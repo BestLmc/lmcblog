b0937209b07f2bfbb90538c42c17bb9ed340f86e
BestLmc <34178847+BestLmc@users.noreply.github.com>
2020-09-15 20:57:52 +0800

    修改菜单

--- src/components/classic.vue
+++ src/components/classic.vue
@@ -3,23 +3,35 @@
     <a-layout>
       <!-- 头部 -->
       <a-layout-header>
-
         <div id="head-menu-box">
           <a-menu v-model="current" mode="horizontal">
-            <a-menu-item key="mail"> <a-icon type="mail" />菜单一</a-menu-item>
-            <a-menu-item key="app" > <a-icon type="appstore" />菜单二</a-menu-item>
+            <!-- 首页 start -->
+            <a-menu-item key="home">
+              <a-icon type="home" />首页
+            </a-menu-item>
+            <!-- 首页 start -->
+
             <a-sub-menu>
-              <span slot="title" class="submenu-title-wrapper"
-                ><a-icon type="setting" />菜单三</span
-              >
-                <a-menu-item key="setting:1">
-                  Option 1
-                </a-menu-item>
+              <span slot="title" class="submenu-title-wrapper">
+                <a-icon type="shop" />货架
+              </span>
+              <a-menu-item key="books">书籍区</a-menu-item>
+              <a-menu-item key="games">游戏区</a-menu-item>
             </a-sub-menu>
-            <a-menu-item key="alipay"><a-icon type="appstore" /> 菜单四 </a-menu-item>
+
+            <a-menu-item key="blog">
+              <a-icon type="book" />博客
+            </a-menu-item>
+
+            <a-sub-menu>
+              <span slot="title" class="submenu-title-wrapper">
+                <a-icon type="experiment" />实验室
+              </span>
+              <a-menu-item key="special-effects">特效</a-menu-item>
+              <a-menu-item key="layout">布局</a-menu-item>
+            </a-sub-menu>
           </a-menu>
         </div>
-
       </a-layout-header>
       <!-- 头部 -->
 
@@ -30,9 +42,7 @@
       <!-- 主体 -->
 
       <!-- 底部 -->
-      <a-layout-footer>
-
-      </a-layout-footer>
+      <a-layout-footer></a-layout-footer>
       <!-- 底部 -->
     </a-layout>
   </div>
@@ -43,25 +53,24 @@ export default {
   name: "classic",
   data() {
     return {
-      current: ['mail'],
+      current: ["mail"],
     };
   },
-  mounted(){
-  	this.initData()
+  mounted() {
+    this.initData();
   },
-  methods:{
-  	initData(){
+  methods: {
+    initData() {
       this.$router.push("home");
-  	}
-  }
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 #head-menu-box {
-  width: 1100px;
-  bottom:0;
+  padding-top: 72px;
 }
 #components-layout-basic {
   margin: 0px;
